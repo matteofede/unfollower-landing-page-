16383a8956dd8d37c471b793d677fb02cfbdcd64
matteofede <matteoborowy@gmail.com>
2026-07-17 15:44:59 +0300

    Add click event listener to CTA buttons

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".cta-btn, .header-btn");
 
+  buttons.forEach(button => {
+    button.addEventListener("click", function () {
+      console.log("CTA clicked");
+    });
+  });
+});
